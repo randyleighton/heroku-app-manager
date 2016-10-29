bb095de078d4610e7c6966e533fd9213beb58fdb
Randy Leighton <randyleighton@yahoo.com>
2016-10-29 09:29:45 -0700

    basic app and welcome controller

---
+++ app/controllers/app_controller.rb
@@ -0,0 +1,26 @@
+class AppController < ActionController::Base
+
+def index
+    @apps = App.all
+end
+
+def new
+
+end
+
+def edit
+end
+
+def create
+end
+
+def show
+end
+
+def update
+end
+
+def destroy
+end
+
+end
